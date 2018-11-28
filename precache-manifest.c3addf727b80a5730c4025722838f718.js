6d497509fbb8bb34e3d58492dc60205fc057f857
Katsiaryna Fedarovich <kathefy215@gmail.com>
2018-11-28 21:25:28 +0100

    Updates

---
+++ precache-manifest.c3addf727b80a5730c4025722838f718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-oil-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c7ca0cdfc3e54ce07fb2",
+    "url": "/react-oil-project/static/js/runtime~main.c7ca0cdf.js"
+  },
+  {
+    "revision": "43ac370443c9980ef309",
+    "url": "/react-oil-project/static/js/main.43ac3704.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-oil-project/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "43ac370443c9980ef309",
+    "url": "/react-oil-project/static/css/main.49f8aa1a.chunk.css"
+  },
+  {
+    "revision": "c8ae1e12dade3a06198a333f73ca83a5",
+    "url": "/react-oil-project/index.html"
+  }
+];
